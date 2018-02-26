9be3fb6e215d044d23ec8539ad6bbb09144be85b
Mohammad Sazzad Hossain Sakib <sazzadhossainsakib@gmail.com>
2018-02-27 00:05:20 +0600

    password reset tested, issue found for: Login with new password dosen't work

--- src/test/java/PasswordResetTest.java
+++ src/test/java/PasswordResetTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.ScreenshotException;
 
 public class PasswordResetTest {
 
@@ -15,18 +16,31 @@ public class PasswordResetTest {
 	
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Syed Mainul Hasan\\Documents\\chromedriver_win32\\chromedriver.exe");
+				"C:\\Users\\hossa\\Desktop\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//url to test
 		driver.get("http://localhost:8080/ewsd/login");
-		Thread.sleep(5000); // Let the user actually see something!
+		Thread.sleep(2000); // Let the user actually see something!
 		captureScreen(driver, "login_page");
 
 		
-		//invalid email and pass entered. try wrong email
+		//click reset password link... 
 		WebElement passwordResetLink = driver.findElement(By.linkText("I forgot my password"));
 		passwordResetLink.click();
+		Thread.sleep(3000); 
+		captureScreen(driver, "enter email to reset password");
 		
+		//send link and input email
+		WebElement inputEmail = driver.findElement(By.name("email"));
+		inputEmail.sendKeys("sazzadhossainsakib@gmail.com");
+		captureScreen(driver, "email enterd for password reset link");
+		
+		//submit button for link
+		WebElement submit = driver.findElement(By.className("btn"));
+		submit.click();
+		Thread.sleep(2000);
+		captureScreen(driver, "link sent to the email successfully");
+		driver.get("https://mail.google.com");
 		driver.quit();
 		
 	}
