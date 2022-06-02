3449b86d02c81790cfb59a6599656a12e0e721b7
mainul35 <mainuls18@gmail.com>
2022-06-03 00:02:21 +0700

    docker-compose and automation script updated

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ if [ $STATUS -eq 0 ]; then
   echo "Deployment package generating Successful"
   sudo docker image rm mainul35/university-qa-board
   sudo docker image build -t mainul35/university-qa-board .
-  sudo docker-compose up
+  sudo docker compose up
 else
   echo "Deployment package generating Failed"
 fi
